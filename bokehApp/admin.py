02338830ae61225ef85a37629ea201d0d04c6524
Chelsea Tomkowiak <tika.xo@gmail.com>
2019-07-13 13:16:35 -0500

    changes - july 13 - still working on importing data from model

--- bokehApp/admin.py
+++ bokehApp/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from bokehApp.models import Products
 from bokehApp.models import CurrentStats
 
-admin.site.register(Products)
 admin.site.register(CurrentStats)
--- bokehApp/views.py
+++ bokehApp/views.py
@@ -5,7 +5,6 @@ from bokeh.models import HoverTool, LassoSelectTool, WheelZoomTool, PointDrawToo
 
 from bokeh.palettes import Category20c, Spectral6
 from bokeh.transform import cumsum
-from .models import Products
 from .models import CurrentStats
 from numpy import pi
 # import pandas as pd
@@ -14,23 +13,28 @@ from bokeh.resources import CDN
 
 def products(request):
     items = ["Incoming Water", "Outgoing Water"]
-    waterInn = CurrentStats.objects.values()[:1]
-#    test = CurrentStats.objects.filter('updated_at').values('waterOut')
-    waterIn = 0
-    waterOut = 175
+    tempp = [CurrentStats.objects.all()]
+    waterInList = []
+    #THIS WORKS    waterOutList = [4, 5, 6]
+    waterOutList = []
+    for CurrentStats.waterIn in tempp:
+        waterInList += [7, 8, 9]
+#    for CurrentStats.waterOut in tempp:
+#        waterOutList += [CurrentStats.waterOut]
+    for e in tempp:
+        waterOutList += e.waterOut
+    numInWaterInList = len(waterInList)
+    numInWaterOutList = len(waterOutList)
+    numMinusOneWaterIn = (numInWaterInList - 1)
+    numMinusOneWaterOut = (numInWaterOutList - 1)
+    currentWaterIn = waterInList[numMinusOneWaterIn]
+    currentWaterOut = waterOutList[numMinusOneWaterOut]
     counts = []
-
-    for i in waterInn:
-        if "waterIn" in i.values():
-            waterIn += CurrentStats.waterIn
-        if "waterOut" in i.values():
-            waterOut += CurrentStats.waterOut
-    counts.extend([waterIn, waterOut])
-
+    counts.extend([currentWaterIn, currentWaterOut])
     plot = figure(x_range=items, plot_height=600, plot_width=600, title="Temperature", toolbar_location="right", tools="pan,wheel_zoom,box_zoom,reset,tap")
     plot.title.text_font_size = '20pt'
     plot.xaxis.major_label_text_font_size = '14pt'
     plot.vbar(items, top=counts, width=.4, color="navy", legend="Current Temperature")
     plot.legend.label_text_font_size = '14pt'
     script, div = components(plot)
-    return render(request, 'starter.html', {'script': script, 'div': div, 'waterInn': waterInn, 'counts': counts, 'items': items, 'waterIn': waterIn, 'waterOut': waterOut})
+    return render(request, 'starter.html', {'script': script, 'div': div, 'waterOutList': waterOutList, 'waterInList': waterInList, 'tempp': tempp, 'counts': counts, 'items': items, 'numInWaterInList': numInWaterInList, 'numInWaterOutList': numInWaterOutList, 'numMinusOneWaterIn': numMinusOneWaterIn, 'numMinusOneWaterOut': numMinusOneWaterOut, 'currentWaterIn': currentWaterIn, 'currentWaterOut': currentWaterOut})
